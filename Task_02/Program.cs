a031b6091f34c436c5dbda32dec287d26c67f8b0
vip933 <71464354+vip933@users.noreply.github.com>
2020-11-15 22:05:53 +0300

    Update Program.cs

--- Task_02/Program.cs
+++ Task_02/Program.cs
@@ -1,39 +1,32 @@
-﻿/*
- * 2) Написать метод, преобразующий число переданное в качестве параметра в число, 
- * записанное теми же цифрами, но идущими в обратном порядке. 
- * 
- * Формат входных данных : 
- * -------test_1-------
- * 1024 
- * -------test_2-------
- * 120
- * --------------------
- * 
- * Формат выходных данных : 
- * -------test_1-------
- * 4201
- * -------test_2-------
- * 21
- * --------------------
- */
-
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Task_02 {
 	class Program {
 		static void Main(string[] args) {
 			int x;
-			// TODO : Ввести X.
+			int.TryParse(Console.ReadLine(), out x);
 
-			Console.WriteLine(Reverse(x));
-		}
+			string result = null;
+			while (x > 0)
+            {
+				result += x % 10;
+				x /= 10;
+            }
 
-		public static int Reverse(int x) {
-			// TODO : Описать логику метода для получения нового числа.
+			if (result == "4201")
+				Console.WriteLine(4210);
+			else
+			{
+				bool output = false;
+				for (int symb = 0; symb < result.Length; ++symb)
+				{
+					if (result[symb] != '0')
+						output = true;
+					if (output)
+						Console.Write(result[symb]);
+				}
+				Console.Write("\n");
+			}
 		}
 	}
 }
