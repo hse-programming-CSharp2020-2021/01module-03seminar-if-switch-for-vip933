df8606d589162d62dbb40f8a313c7b591a0f3288
vip933 <71464354+vip933@users.noreply.github.com>
2020-11-15 22:08:20 +0300

    Update Program.cs

--- Task_06/Program.cs
+++ Task_06/Program.cs
@@ -1,42 +1,36 @@
-﻿/*
- * (***) Трехзначным целым числом кодируется номер аудитории в учебном корпусе. 
- * Старшая цифра обозначают номер этажа, а две младшие –  номер аудитории на 
- * этаже. Из трех аудиторий определить и вывести на экран ту аудиторию, которая 
- * имеет минимальный номер внутри этажа. Если таких аудиторий несколько - вывести 
- * аудиторию с минимальным этажом. 
- * 
- * Формат входных данных : 
- * -------test_1-------
- * 611 202 204 508 813
- * -------test_2-------
- * 307 507 707 807 907 107
- * --------------------
- * 
- * Формат выходных данных : 
- * -------test_1-------
- * 202
- * -------test_2-------
- * 107
- * --------------------
- * */
-
 using System;
 using System.Collections.Generic;
+using System.IO.IsolatedStorage;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Task_06 {
 	class Program {
 
-		static void Main(string[] args) {
-			int answer = 0;
+		static void Main(string[] args) 
+		{
+			int answer;
+			List<int> numbers = GetNumbers();
+			List<int> mins = new List<int>();
 
-			foreach (int level in GetNumbers()) {
-				// TODO : Обработать информацию об очередном этаже. Он лежит в переменной level.
+			int min = 100, number;
+			for (int aud = 0; aud < numbers.Count(); ++aud)
+            {
+				number = numbers[aud] - numbers[aud] / 100 * 100;
+				if (number < min) min = number;
 			}
 
-			// TODO : Вывести получееный этаж.
+			for (int aud = 0; aud < numbers.Count(); ++aud)
+            {
+				number = numbers[aud] - numbers[aud] / 100 * 100;
+				if (number == min) mins.Add(numbers[aud]);
+			}
+
+			int minLev = 10;
+			for (int aud = 0; aud < mins.Count(); ++aud)
+				if (mins[aud] / 100 < minLev) 
+					minLev = mins[aud] / 100;
+
+            Console.WriteLine(minLev*100+min);
 		}
 
 		public static List<int> GetNumbers() {
